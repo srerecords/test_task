75f04634e774e27fdc20f2fb32efb4bf1e9c20c4
Oleksii Dvuila <oleksii.dvuila@verbit.ai>
2025-11-15 00:14:48 +0200

    Deployed S3 bucket

--- variables.tf
+++ variables.tf
@@ -1,5 +1,16 @@
+variable "env" {
+  description = "Environment of deployment"
+  type        = string
+  default     = "test"
+}
 variable "region" {
   description = "AWS region for services"
   type        = string
   default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "S3 bucket name"
+  type        = string
+  default     = "saved-files-7777"
 }
